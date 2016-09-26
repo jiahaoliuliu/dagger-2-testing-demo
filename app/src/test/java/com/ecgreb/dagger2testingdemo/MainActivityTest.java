f574d8b0407a6d09768621100b6b0477660367e7
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-09-26 21:39:22 +0400

    Created 20 tests

--- app/src/test/java/com/ecgreb/dagger2testingdemo/MainActivityTest.java
+++ app/src/test/java/com/ecgreb/dagger2testingdemo/MainActivityTest.java
@@ -1,13 +1,8 @@
 package com.ecgreb.dagger2testingdemo;
 
-import org.bouncycastle.jce.provider.symmetric.ARC4;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import android.widget.TextView;
 
@@ -20,13 +15,104 @@ public class MainActivityTest extends BaseTest{
         mainActivity = Robolectric.setupActivity(MainActivity.class);
     }
 
-    @Test public void shouldInjectMockStringFactory() throws Exception {
+    @Test public void shouldInjectMockStringFactory1() throws Exception {
         TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
         assertEquals("Fake greeting", greeting.getText());
     }
 
-    @Test public void shouldInjectMagicNumber() throws Exception {
+    @Test public void shouldInjectMagicNumber1() throws Exception {
         TextView number = (TextView) mainActivity.findViewById(R.id.number);
         assertEquals("The magic number is 4.", number.getText());
     }
+
+    @Test public void shouldInjectMockStringFactory2() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber2() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory3() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber3() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory4() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber4() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory5() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber5() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory6() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber6() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory7() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber7() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory8() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber8() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory9() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber9() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
+    @Test public void shouldInjectMockStringFactory10() throws Exception {
+        TextView greeting = (TextView) mainActivity.findViewById(R.id.greeting);
+        assertEquals("Fake greeting", greeting.getText());
+    }
+
+    @Test public void shouldInjectMagicNumber10() throws Exception {
+        TextView number = (TextView) mainActivity.findViewById(R.id.number);
+        assertEquals("The magic number is 4.", number.getText());
+    }
+
 }
