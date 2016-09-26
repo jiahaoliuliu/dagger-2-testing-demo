44b9fed0d476f8b7e7a8b533e4dd1723b01a21d4
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-09-26 21:03:10 +0400

    Duplicate till 500 tests

--- app/src/test/java/com/ecgreb/dagger2testingdemo/MainActivityTest12.java
+++ app/src/test/java/com/ecgreb/dagger2testingdemo/MainActivityTest12.java
@@ -1,20 +1,19 @@
 package com.ecgreb.dagger2testingdemo;
 
+import android.widget.TextView;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-
-import android.widget.TextView;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 23)
-public class MainActivityTest {
+public class MainActivityTest12 {
     private MainActivity mainActivity;
 
     @Before public void setUp() throws Exception {
