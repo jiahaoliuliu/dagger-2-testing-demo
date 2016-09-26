72923c5d7c7a357b6522eba14714781817b8e8e5
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-09-26 22:01:04 +0400

    Using robolectric3.1.1

--- app/src/test/java/com/ecgreb/dagger2testingdemo/BaseTest.java
+++ app/src/test/java/com/ecgreb/dagger2testingdemo/BaseTest.java
@@ -2,13 +2,14 @@ package com.ecgreb.dagger2testingdemo;
 
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 /**
  * Created by jiahao on 26/09/16.
  */
 
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 23)
 public abstract class BaseTest {
 }
